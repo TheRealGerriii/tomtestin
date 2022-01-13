34151d9d4e3f4d94068113c324751b8039c4150d
TheRealGerriii <hermanyi-gergely@hotmail.com>
2022-01-13 10:10:07 +0100

    kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
+/*
+File: app.component.ts
+Author: Hermányi Gergely
+Copyright: 2022, Hermányi Gergely
+Group: Szoft II N
+Date: 2022-01-13
+Github: https://github.com/TheRealGerriii
+Licenc: GNU GPL
+*/
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tomtestin';
+
+  eredmenyVisible = false;
+
+  calcForm !: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.calcForm = this.formBuilder.group({
+      magassag: [''],
+      testomeg: [''],
+      testomegindex: [''],
+    });
+  }
+
+  onClickSzamit() {
+    let magassag = Number(this.calcForm.value.magassag)/100;
+    let testomeg = Number(this.calcForm.value.testomeg);
+
+    let testomegindex = testomeg / Math.pow(magassag, 2);
+    this.calcForm.patchValue({testomegindex: testomegindex});
+    this.eredmenyVisible = true;
+  }
 }
